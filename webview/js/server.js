082b71a2ca7c78c77260e30f0934539cad9a1e70
yeongwoojang <52729223+yeongwoojang@users.noreply.github.com>
2021-03-02 00:05:28 +0900

    Update server.js

--- webview/js/server.js
+++ webview/js/server.js
@@ -2,10 +2,7 @@ let express = require('express');
 let app = express();
 
 const path = require('path');
-// app.use(express.static(path.join(__dirname, 'html')));
-console.log(__dirname)
 app.use(express.static(path.join(__dirname,'../','public')));
-app.set("view engine", "ejs");
 app.get('/',(req,res)=>{
  res.sendFile(path.join(__dirname,'../','index.html'));
 });
